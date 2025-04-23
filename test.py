bab3f6dee3c93861baed834b9d2a6d0c73ca7ab2
SirCrabapple99 <99812880+SirCrabapple99@users.noreply.github.com>
2025-04-23 15:54:51 -0500

    corrected offset

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ faces = [[1, 2, 3, 4], [1, 2, 6, 5], [1, 4, 8, 5], [3, 4, 8, 7], [5, 6, 7, 8], [
 faceverts = []
 newverts = []
 adjustedverts = []
-rotation = [0, 0, 1]
+offset = [[0, 0], [0, 0], [0, 0]]
 cubegroup = [Rect(200, 200, 200, 200)]
 
 focal = 200
@@ -15,7 +15,7 @@ def cubeinit():
     global faces
     global faceverts
     global adjustedverts
-    global rotation
+    global offset
     global cubegroup
     global focal
     global newverts
@@ -99,6 +99,7 @@ def rotatex(amountdeg):
     global baseverts
     global faces
     global faceverts
+    global offset
     global newverts
     global adjustedverts
     global cubegroup
@@ -110,12 +111,17 @@ def rotatex(amountdeg):
         x += 1
         for f in i:
             c += 1
+            newverts[x][c][0] -= offset[0][0]
+            newverts[x][c][1] -= offset[1][0]
+            newverts[x][c][2] -= offset[2][0]
             xx = newverts[x][c][0]
             zz = newverts[x][c][1]
             yy = newverts[x][c][2]
-            newverts[x][c][1] = ((yy * math.cos(deg)) - (zz * math.sin(deg)))
-            newverts[x][c][2] = ((yy * math.sin(deg)) + (zz * math.cos(deg)))
-            
+            newverts[x][c][1] = ((yy * math.sin(deg)) + (zz * math.cos(deg)))
+            newverts[x][c][2] = ((yy * math.cos(deg)) - (zz * math.sin(deg)))
+            newverts[x][c][0] += offset[0][0]
+            newverts[x][c][1] += offset[1][0]
+            newverts[x][c][2] += offset[2][0]
     cube()
 # rotate around y axis
 def rotatey(amountdeg):
@@ -124,6 +130,7 @@ def rotatey(amountdeg):
     global faces
     global faceverts
     global newverts
+    global offset
     global adjustedverts
     global cubegroup
     global focal
@@ -134,18 +141,24 @@ def rotatey(amountdeg):
         x += 1
         for f in i:
             c += 1
+            newverts[x][c][0] -= offset[0][0]
+            newverts[x][c][1] -= offset[1][0]
+            newverts[x][c][2] -= offset[2][0]
             xx = newverts[x][c][0]
             zz = newverts[x][c][1]
             yy = newverts[x][c][2]
             newverts[x][c][0] = (((xx * math.cos(deg)) + (zz * math.sin(deg))))
             newverts[x][c][1] = (((-1 * xx) * math.sin(deg)) + (zz * math.cos(deg)))
-            
+            newverts[x][c][0] += offset[0][0]
+            newverts[x][c][1] += offset[1][0]
+            newverts[x][c][2] += offset[2][0]
     cube()
 # rotate around z axis
 def rotatez(amountdeg):
     cube()
     global baseverts
     global faces
+    global offset
     global faceverts
     global newverts
     global adjustedverts
@@ -158,22 +171,33 @@ def rotatez(amountdeg):
         x += 1
         for f in i:
             c += 1
+            newverts[x][c][0] -= offset[0][0]
+            newverts[x][c][1] -= offset[1][0]
+            newverts[x][c][2] -= offset[2][0]
             xx = newverts[x][c][0]
             zz = newverts[x][c][1]
             yy = newverts[x][c][2]
             newverts[x][c][0] = ((xx * math.cos(deg)) - (yy * math.sin(deg)))
             newverts[x][c][2] = ((xx * math.sin(deg)) + (yy * math.cos(deg)))
+            newverts[x][c][0] += offset[0][0]
+            newverts[x][c][1] += offset[1][0]
+            newverts[x][c][2] += offset[2][0]
     cube()
 def move(amount, xx, yy, zz):
+    global offset
+    global newverts
     x = -1
+    offset[0][0] += amount * xx
+    offset[1][0] += amount * zz
+    offset[2][0] -= amount * yy
     for i in newverts:
         c = -1
         x += 1
-        for f in newverts[x]:
+        for f in i:
             c += 1
             newverts[x][c][0] += amount * xx
             newverts[x][c][1] += amount * zz
-            newverts[x][c][2] += (-1 * amount) * yy
+            newverts[x][c][2] -= amount * yy
     cube()
 def onKeyHold(keys):
     for i in keys:
@@ -194,10 +218,9 @@ def onKeyHold(keys):
         if i == 'left':
             rotatey(5)
         if i == 'up':
-            rotatez(5)
+            rotatex(-5)
         if i == 'down':
-            rotatez(-5)
-
+            rotatex(5)
 app.stepsPerSecond = 30;
 # def onStep():
 #     rotatez(1)
@@ -205,4 +228,6 @@ app.stepsPerSecond = 30;
 #     choice = [-1, 0, 1]
 #     move(5, rounded(random.choice(choice)), rounded(random.choice(choice)),rounded(random.choice(choice)))
 cubeinit()
+rotatex(25)
+
 cube()
